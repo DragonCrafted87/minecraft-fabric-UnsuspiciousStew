d2c3b161f345d3712a59284baeaede1e651d606b
DragonCrafted87 <24697493+DragonCrafted87@users.noreply.github.com>
2022-06-04 10:34:55 -0500

    filename prodding

--- src/main/java/schauweg/unsuspiciousstew/mixin/MixinInGameHud.java
+++ src/main/java/schauweg/unsuspiciousstew/mixin/MixinInGameHud.java
@@ -7,10 +7,8 @@ import net.fabricmc.api.Environment;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.font.TextRenderer;
 import net.minecraft.client.gui.hud.InGameHud;
-import net.minecraft.client.item.TooltipContext;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.effect.StatusEffect;
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.nbt.NbtCompound;
@@ -20,15 +18,10 @@ import net.minecraft.util.ChatUtil;
 import net.minecraft.util.Formatting;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-import schauweg.unsuspiciousstew.StewInfo;
-
-import java.util.List;
 
 import static net.minecraft.client.gui.DrawableHelper.fill;
 
